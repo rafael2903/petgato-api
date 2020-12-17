edde3bf646fde86a665896b8b3da8016111941da
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-12-17 17:12:41 -0300

    feat: generate models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :tags
   resources :contacts, only: [:index, :show, :create, :destroy]
   resources :users
+  resources :posts
   post '/recovery', to: 'users#password_reset'
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_003049) do
+ActiveRecord::Schema.define(version: 2020_12_17_190915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comment_has_replies", force: :cascade do |t|
+    t.bigint "reply_id", null: false
+    t.bigint "comment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["comment_id"], name: "index_comment_has_replies_on_comment_id"
+    t.index ["reply_id"], name: "index_comment_has_replies_on_reply_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_has_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_post_has_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_has_tags_on_tag_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.integer "views"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tag_posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -39,4 +102,12 @@ ActiveRecord::Schema.define(version: 2020_12_16_003049) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comment_has_replies", "comments"
+  add_foreign_key "comment_has_replies", "replies"
+  add_foreign_key "post_has_tags", "posts"
+  add_foreign_key "post_has_tags", "tags"
+
+  add_foreign_key "tags", "posts"
+  add_foreign_key "post", "tags"
 end
